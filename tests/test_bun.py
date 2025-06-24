6679eb8e1d8b6ecef69e2be4b659bec5380912df
Max <i@gwein.ru>
2025-06-24 22:07:06 +0300

    Задание 1: исправления

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,20 +1,21 @@
 import pytest
 from praktikum.bun import Bun
 
-def test_bun_init():
-    bun = Bun("Test Bun", 1.99)
-    assert bun.name == "Test Bun"
+class TestBun:
+    def test_bun_init(self):
+        bun = Bun("Test Bun", 1.99)
+        assert bun.name == "Test Bun"
 
-def test_bun_price():
-    bun = Bun("Test Bun", 1.99)
-    assert bun.price == 1.99
+    def test_bun_price(self):
+        bun = Bun("Test Bun", 1.99)
+        assert bun.price == 1.99
 
-@pytest.mark.parametrize("name, price", [("Black Bun", 100), ("White Bun", 200)])
-def test_bun_get_name_returns_correct(name, price):
-    bun = Bun(name, price)
-    assert bun.get_name() == name
+    @pytest.mark.parametrize("name, price", [("Black Bun", 100), ("White Bun", 200)])
+    def test_bun_get_name_returns_correct(self, name, price):
+        bun = Bun(name, price)
+        assert bun.get_name() == name
 
-@pytest.mark.parametrize("name, price", [("Black Bun", 100), ("White Bun", 200)])
-def test_bun_get_price_returns_correct(name, price):
-    bun = Bun(name, price)
-    assert bun.get_price() == price
+    @pytest.mark.parametrize("name, price", [("Black Bun", 100), ("White Bun", 200)])
+    def test_bun_get_price_returns_correct(self, name, price):
+        bun = Bun(name, price)
+        assert bun.get_price() == price
